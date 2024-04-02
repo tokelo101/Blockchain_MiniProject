4b222ae4bac09d7aa4d9c91821d7f7ce54f1ebe0
tokelo101 <tokelomonare@gmail.com>
2024-04-03 00:00:53 +0200

    Main Page and Register Page Created

--- src/GUI/HomePane.java
+++ src/GUI/HomePane.java
@@ -1,38 +1,93 @@
 package GUI;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class HomePane extends StackPane {
     private VBox mainBox;
     private HBox searchBox;
+    private HBox subBox; //Contains the navigation Box and the Content Box 
+    private VBox navBox;
+    private Pane content;
     private TextField txtSearchBox;
     private Button btnSearchButton;
-    private Button btnRegister;
+    
+    //Navigation items
+    private Button navSettings;
+    private Button navUploadSong;
+    private Button navSongList;
+    private Button navUpdateLicenseTerms;
+    private Button navRegister;
+    private Button navLogout;
+    private Button navLogin;
+    
+    //Navigation Contents [Panes]
     private RegisterPane register;
-    public HomePane(){
-    	btnRegister = new Button("Register");
-        mainBox = new VBox();
-        
+    
+    public HomePane() {
+    	
+    	mainBox = new VBox();
         mainBox.setAlignment(Pos.TOP_CENTER);
+        
+        subBox = new HBox();
+        content = new Pane();
+        
         searchBox = new HBox();
         searchBox.setAlignment(Pos.CENTER);
+        searchBox.setPadding(new Insets(10,10,10,10));
+        
         txtSearchBox = new TextField();
         txtSearchBox.setPromptText("search");
-        btnSearchButton = new Button("Serach Icon");
+        
+        btnSearchButton = new Button("Search Icon");
         searchBox.getChildren().addAll(txtSearchBox, btnSearchButton);
-        mainBox.getChildren().addAll(searchBox, btnRegister);
-        this.getChildren().add(mainBox);
         
         
-        btnRegister.setOnAction(event->{
+        //Navigation Bar
+        navBox = new VBox();
+        navBox.setPadding(new Insets(10,10,10,10));
+        navBox.setSpacing(10);
+        
+        navRegister = new Button("Register");
+        navRegister.setPrefWidth(200);
+        navSettings = new Button("Settings");
+        navSettings.setPrefWidth(200);
+        navUploadSong = new Button("Upload Song");
+        navUploadSong.setPrefWidth(200);
+        navSongList = new Button("Song List");
+        navSongList.setPrefWidth(200);
+        navUpdateLicenseTerms = new Button("Update License Terms");
+        navUpdateLicenseTerms.setPrefWidth(200);
+        navLogin = new Button("Login");
+        navLogin.setPrefWidth(200);
+        navLogout = new Button("Logout");
+        navLogout.setPrefWidth(200);
+        
+        navBox.getChildren().addAll(navRegister, navSettings, navUploadSong, navSongList, navUpdateLicenseTerms, navLogin, navLogout);
+        
+        content.setPrefWidth(500);
+        content.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null)));
+        subBox.getChildren().addAll(navBox, content);
+        
+        mainBox.getChildren().addAll(searchBox, subBox);
+        this.getChildren().addAll(mainBox);
+        
+        
+        navRegister.setOnAction(event->{
         	register = new RegisterPane();
         	this.getChildren().remove(0);
-        	mainBox.getChildren().addAll(register);
+        	content.getChildren().addAll(register);
         	this.getChildren().add(mainBox);
         });
     }
--- src/GUI/RegisterPane.java
+++ src/GUI/RegisterPane.java
@@ -1,40 +1,63 @@
 package GUI;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class RegisterPane extends StackPane {
-	private VBox frmRegister;
-	private HBox frmDetails;
-	private Label lbRegister;
-	private Label lbName;
-	private Label lbSurname;
-	private Label lbEmail;
-	private Label lbPassword;
-	private Label lbConfirmPassword;
-	private Button btnRegister;
-	
-
 	
 	public RegisterPane() {
-		frmRegister = new VBox();
-		frmDetails = new HBox();
-		
-		lbRegister = new Label("Register");
-		lbName = new Label("Name");
-		lbSurname = new Label("Surname");
-		lbEmail = new Label("Email");
-		lbPassword = new Label("Password");
-		lbConfirmPassword = new Label("Confirm Password");
-		btnRegister = new Button("Register");
-		
-		frmDetails.getChildren().addAll(lbName, lbSurname);
-		frmRegister.getChildren().addAll(lbRegister, frmDetails, btnRegister);
-		
-		frmDetails.setAlignment(Pos.CENTER);
-		frmRegister.setAlignment(Pos.CENTER);
-		this.getChildren().add(frmRegister);
+
+		 GridPane gridPane = new GridPane();
+	        gridPane.setAlignment(Pos.CENTER);
+	        gridPane.setHgap(10);
+	        gridPane.setVgap(10);
+	        gridPane.setPadding(new Insets(25));
+
+	        // Add form elements
+	        Label lbTitle = new Label("Register Form");
+	        lbTitle.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+	        gridPane.add(lbTitle, 0, 0, 2, 1);
+	        GridPane.setHalignment(lbTitle, HPos.CENTER);
+
+	        Label lbStagename = new Label("Stagename:");
+	        gridPane.add(lbStagename, 0, 1);
+
+	        TextField txtStagename = new TextField();
+	        gridPane.add(txtStagename, 1, 1);
+	        
+	        Label lbName = new Label("Name:");
+	        gridPane.add(lbName, 0, 2);
+
+	        TextField txtName = new TextField();
+	        gridPane.add(txtName, 1, 2);
+	        
+	        Label lbSurname = new Label("Surname:");
+	        gridPane.add(lbSurname, 0, 3);
+
+	        TextField txtSurname = new TextField();
+	        gridPane.add(txtSurname, 1, 3);
+
+	        Label lbEmail = new Label("Email:");
+	        gridPane.add(lbEmail, 0, 4);
+
+	        TextField txtEmail = new TextField();
+	        gridPane.add(txtEmail, 1, 4);
+
+	        Label lbPassword = new Label("Password:");
+	        gridPane.add(lbPassword, 0, 5);
+
+	        PasswordField password = new PasswordField();
+	        gridPane.add(password, 1, 5);
+
+	        Button btnRegister = new Button("Register");
+	        gridPane.add(btnRegister, 0, 6, 2, 1);
+	        GridPane.setHalignment(btnRegister, HPos.CENTER);
+	        this.getChildren().add(gridPane);
 	}
 	
 }
